ef600bb92abb1d619c3d7032aa223f99610a5375
Kazushi Sorakubo <sorakubo1123@gmail.com>
2020-02-11 09:44:11 +0900

    Create assessment.js

---
+++ assessment.js
@@ -0,0 +1,15 @@
+'use strict';
+const userNameInput = document.getElementById('user-name');
+const assessmentButton = document.getElementById('assessment');
+const resultDivided = document.getElementById('result-area');
+const tweetDivided = document.getElementById('tweet-area');
+
+/**
+ * 指定した要素の子どもを全て除去する
+ * @param {HTMLElement} element HTMLの要素
+ */
+function removeAllChildren(element) {
+  while (element.firstChild) { // 子どもの要素があるかぎり除去
+    element.removeChild(element.firstChild);
+  }
+}
